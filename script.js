51dd79caf30339f2f2b6d078addafbb7d8b09ae3
AGN <itzme907@gmail.com>
2023-05-17 17:19:20 +0300

    Add hoverStyling function

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-const container = document.querySelector('.container')
 
-const gridSize = 16
-let width = 600 / gridSize
-for (let i=0; i < gridSize * gridSize; i++) {
-        const div = document.createElement('div')
-        div.classList.add('square')
-        div.style.width = `${width}px`
-        div.style.height = `${width}px`
-        
-        container.appendChild(div)
-        
+
+function createGrids(gridSize) {
+    // Set Specify chosen grid size
+    let gridTotal = document.querySelector('#grid-total')
+    gridTotal.textContent = gridSize
+
+
+    let width = 700 / gridSize
+    for (let i = 0; i < gridSize * gridSize; i++) {
+
+        let cell = document.createElement('div')
+
+        cell.classList.add('cell')
+        cell.style.width = `${width}px`
+        cell.style.height = `${width}px`
+
+        container.appendChild(cell)
     }
-    
 
 
-const divs = document.querySelectorAll('.square')
+    // Style background-color when mouse hover ends
+    hoverStyling()
 
-divs.forEach(item => item.addEventListener('mouseover', e => {
-    e.target.classList.add('black') 
-}, {capture: true}))
+    // Remove all background styling
+    const restart = document.querySelector('#restart')
+    restart.addEventListener('click', removeBackgroundColor)
+}
+
+
+function hoverStyling() {
+    let cells = document.querySelectorAll('.cell')
+    let cellColor = document.querySelector('#cell-color')
+    let rgb = document.querySelector('#rgb')
+
+    cells.forEach(item => item.addEventListener('mouseout', e => {
+        let cellStyle = e.target.style;
+
+        if (!cellStyle.backgroundColor) {
+            cellStyle.backgroundColor = (rgb.checked) ? randomColor() : cellColor.value
+
+        }
+
+    }))
+
+}
+
+
+
+
+
+const container = document.querySelector('.container')
+const gridRange = document.querySelector('#grid-count')
+
+
+
+createGrids(gridRange.value)
+
+
+
+
+
+
